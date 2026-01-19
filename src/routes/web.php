dd2078dc83aee4bb90af73b04703ca14a57621d3
jampasnando <jampasnando@gmail.com>
2026-01-19 16:22:58 -0400

    feat(routes) a raiz sin market

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,13 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarritoController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/', [App\Http\Controllers\MarketController::class, 'index'])->name('market.index');
-Route::get('market', [App\Http\Controllers\MarketController::class, 'index'])->name('market.index');
-Route::get('buscar', [App\Http\Controllers\MarketController::class, 'buscar'])->name('market.buscar');
-// Route::post('buscar',function(){
-//     echo "hola";
-// });
-Route::post('registrarcarrito', [CarritoController::class, 'registrarCarrito'])->name('registrarcarrito');
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route::get('/', [App\Http\Controllers\MarketController::class, 'index'])->name('market.index');
+// Route::get('market', [App\Http\Controllers\MarketController::class, 'index'])->name('market.index');
+// Route::get('buscar', [App\Http\Controllers\MarketController::class, 'buscar'])->name('market.buscar');
+// Route::post('registrarcarrito', [CarritoController::class, 'registrarCarrito'])->name('registrarcarrito');
