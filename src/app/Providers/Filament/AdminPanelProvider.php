870d3a9b8765b7b22e69caeff0a1e9bacc0f3f38
jampasnando <jampasnando@gmail.com>
2026-01-19 16:27:27 -0400

    feat(AdminPanel) color light no widgets

--- src/app/Providers/Filament/AdminPanelProvider.php
+++ src/app/Providers/Filament/AdminPanelProvider.php
@@ -34,6 +34,7 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->defaultTheme('light')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
@@ -41,8 +42,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
-                AccountWidget::class,
-                FilamentInfoWidget::class,
+                // AccountWidget::class,
+                // FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
